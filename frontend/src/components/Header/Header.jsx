d92f5b61776bae8d6db873a453559716807839f4
burak <burakoncuy@gmail.com>
2025-01-14 16:01:39 -0500

    update some css

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,27 +1,45 @@
-import { Link, NavLink } from 'react-router-dom'; //comment added for push
+import { Link } from 'react-router-dom';
 import ProfileButton from '../Navigation/ProfileButton';
 import './Header.css';
+import { useState } from 'react';
+
+// Function to generate a new random number between 1 and 8
+const generateRandomNumber = () => Math.floor(Math.random() * 8) + 1;
+
 
 const Header = ({ isLoaded, sessionUser }) => {
+     // State to store the current random spot ID
+  const [randomSpotId, setRandomSpotId] = useState(generateRandomNumber());
+
+  // Function to handle click and update the random spot ID
+  const handleRandomClick = () => {
+    setRandomSpotId(generateRandomNumber());
+  };
+
   return (
     <header className="header">
-      <Link to="/">
-        <img src="/logo.png" alt="Lodging Logo" className="logo" />
-      </Link>
-      <nav className="nav">
-        <ul className="navigation">
-          {sessionUser && (
-            <li>
-              <NavLink to="/spots/new" className="create-new-spot-link">Create a New Spot</NavLink>
-            </li>
-          )}
-          {isLoaded && (
-            <li>
-              <ProfileButton user={sessionUser} />
-            </li>
-          )}
-        </ul>
-      </nav>
+      <div className="logo-container">
+        <Link to="/">
+          <img src="/logo.png" alt="Lodging Logo" className="logo" />
+        </Link>
+      </div>
+      <div className="center-text">
+      <Link 
+          to={`/spots/${randomSpotId}`} 
+          className="center-text-link" 
+          onClick={handleRandomClick}
+        >
+          belong anywhere
+        </Link>
+      </div>
+      <div className="profile-section">
+        {isLoaded && <ProfileButton user={sessionUser} className="profile-button" />}
+        {sessionUser && (
+          <Link to="/spots/new" className="create-new-spot-link">
+            Create a New Spot
+          </Link>
+        )}
+      </div>
     </header>
   );
 };
